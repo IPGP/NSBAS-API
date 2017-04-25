e8514c8cba27418db7fb9b4c3bb090536eb52d5a
Franck <Franck@uga>
2017-04-25 10:04:44 +0200

    ws_compInterf: ajout cmd + recup donnees a partir du json

--- api_nsbas/bin/ws_compInterf.py
+++ api_nsbas/bin/ws_compInterf.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 # Webservice ws_compInterf , ex WS4
 #
-# Fonction : 
+# Fonction :
 # Calculer les interférogrammes.
 #
 # Ce code est inspiré de https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 # Pour en savoir plus : http://flask.pocoo.org/docs/0.12/quickstart/
 #
-# Utilisation des arguments : 
+# Utilisation des arguments :
 # request.json est un hypercube associatif qui reprend la structure du json envoye.
 # request.values est un tableau associatif qui reprend les variables transmises en mode key-value pair (?toto=156&mode=sync)
 #
 # Tests :
-# Tester 
+# Tester
 # Execute avec curl -i -umiguel:python -H "Content-Type: application/json" -X POST -d '{"processToken" : "f5e6f9g8t5232n5d5d6s56" , "subSwath" : "2"}' http://gravi155.step.univ-paris-diderot.fr:5026/v1.0/services/ws_compInterf?mode=async
-# 
+#
 # GetResult : curl -i -umiguel:python -X GET http://gravi155.step.univ-paris-diderot.fr:5026/v1.0/services/ws_compInterf/5698/67b373c6-4c6f-443b-8cb0-0986d7c76598/outputs
 # GetStatus : curl -i -umiguel:python -X GET http://gravi155.step.univ-paris-diderot.fr:5026/v1.0/services/ws_compInterf/5698/67b373c6-4c6f-443b-8cb0-0986d7c76598
 # GetCapabilities : curl -i -umiguel:python -X GET http://gravi155.step.univ-paris-diderot.fr:5026/v1.0/services
@@ -69,7 +69,7 @@ def get_password(username):
 def unauthorized():
     return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
     # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
-    
+
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
@@ -87,25 +87,25 @@ def get_capabilities():
 @auth.login_required
 def describe_process():
     return jsonify( {
-		  "id": ""+wsName+"",
-		  "label": "ForM@Ter/Etalab ws_compInterf webservice",
-		  "description": "Computes the interferograms on the cluster",
-		  "inputs":  [
-		              {"processToken" : "<token>",
+      "id": ""+wsName+"",
+      "label": "ForM@Ter/Etalab ws_compInterf webservice",
+      "description": "Computes the interferograms on the cluster",
+      "inputs":  [
+              {"processToken" : "<token>",
                                "subSwath" : "<subswathnb>"}
-                             ], 
+                             ],
                   "outputs": [
                               {"jobId" : "<jobId>",
                                "processToken" : "<token>",
                                "resNames" :[
-                                            {"resName" : "<resName1>"},        
+                                            {"resName" : "<resName1>"},
                                             {"resName" : "<resName2>"},
                                             {"<etc...>" : "<etc...>"}
                                            ]
                               }
-	                     ] 
+                         ]
                     }
-		  )
+		)
 
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>/<process_token>', methods = ['GET'])
 @auth.login_required
@@ -119,12 +119,17 @@ def execute():
 # L'execute synchrone renvoit le resultat et la reponse http 200 : OK
 # L'execute asynchrone doit renvoyer la reponse du GetStatus et la reponse http 201 ou celle du GetResult et la reponse http 200, selon
 #
-# L'execute du webservice ws_compInterf doit 
+# L'execute du webservice ws_compInterf doit
 #
-    
+
     if request.values['mode'] == "async" :
         # TODO : estimer dynamiquement walltime
-        process_ressources = {"nodes" : 1, "cores" : 1, "walltime" : "00:50:00", "workdir": remote_prefix}
+        token_dir = config['clstrBaseDir'] + '/' + process_token
+        process_ressources = {"nodes" : 1, "cores" : 4, "walltime" : "01:50:00", "workdir": token_dir}
+        process_token = request.json['processToken']
+        subswath = request.json['subSwath']
+        logging.critical("getting: token %s swath %s", str(process_token), str(subswath))
+
         ret = "Error"
         error = "OK"
         job_id = -1
@@ -137,15 +142,7 @@ def execute():
             logging.critical("unable to log on %s, ABORTING", config["clstrHostName"])
             raise ValueError("unable to log on %s, ABORTING", config["clstrHostName"])
         logging.info("connection OK")
-        """
-        TODO : élaborer la commande correspondant au ws_compInterf
-        token_dir = config['clstrBaseDir'] + '/' + process_token
-        dem_dir = token_dir + '/DEM'
-        slc_dir = token_dir + '/SLC'
-        command = " ".join(["mkdir", dem_dir + '; ',
-                            "nsb_getDemFile.py", "fromRadarImage",
-                            slc_dir, dem_dir])
-        """
+        command = " " .join(["nsb_gen_phase.mpi", "nsbas.proc"])
         try:
             logging.critical("launching command: %s", command)
             job_id = lws_connect.run_on_cluster_node(ssh_client, command, str(process_token),
@@ -155,10 +152,10 @@ def execute():
             error = error + "fail to run command on server: {}".format(excpt)
             logging.error(error)
         ssh_client.close()
-        
+
         # Des lors qu'il est lance, le webservice donne son jeton via son GetStatus, sans attendre d'avoir terminé
         statusJson = getJobStatus(job_id,request.json['processToken'])
-        return jsonify(statusJson), 201        
+        return jsonify(statusJson), 201
     else :
         # En mode synchrone, le webservice donne illico sa réponse GetResult
         # TODO : renseigner les resNames ou leur donner une valeur clairement générique
@@ -168,8 +165,8 @@ def execute():
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>/<process_token>/outputs', methods = ['GET'])
 #@auth.login_required
 def get_result(job_id,process_token):
-    # Lorsqu'il est interrogé uniquement à fin de suivi, 
-    # le webservice a besoin du job Id et, par sécurité, 
+    # Lorsqu'il est interrogé uniquement à fin de suivi,
+    # le webservice a besoin du job Id et, par sécurité,
     # du jeton de suivi du processus de calcul pour répondre
     # On les trouve dans les paramètres de l'url
     # TODO : renseigner les resNames ou leur donner une valeur clairement générique
@@ -182,7 +179,7 @@ def dismiss(job_id):
 # Directive prevue mais non mise en place. Informons l'interlocuteur par le code 501 : NOT IMPLEMENTED
     return jsonify( { 'job_id' : job_id , 'result': False } ), 501
 
-    
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     print "hostname=", config['wsHostName'], "port=", wsPortNumber
