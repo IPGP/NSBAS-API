5823a47f1dbf32f58d8492ef553233459459753b
Franck <Franck@uga>
2017-04-28 12:11:45 +0200

    ws_filtrunwrInterf.py: initial beta version

--- api_nsbas/bin/ws_filtrunwrInterf.py
+++ api_nsbas/bin/ws_filtrunwrInterf.py
@@ -1,6 +1,6 @@
-#!flask/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Webservice ws_filtrunwrInterf , ex WS6
+# Webservice ws_filtrunwrInterf.py
 #
 """Fonction : 
 Aplatir, filtrer et dérouler les interférogrammes calculés. 
@@ -17,7 +17,6 @@ Note PHA 20170418 : Code dérivé du modèle asynchrone ws_coregListInterf en vu
 
 import os
 import logging
-
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
 import paramiko
@@ -31,6 +30,7 @@ import lib_ws.ws_connect as lws_connect
 import parametres
 config = parametres.configdic
 remote_prefix = config["clstrBaseDir"]
+remote_data_prefix = config["clstrDataDir"]
 ssh_config_file = os.environ["HOME"] + "/" + ".ssh/config"
 
 # Preparons la connexion ssh via Paramiko
@@ -41,14 +41,15 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # A restreindre dès que l'hébergement du frontal sera connu
 from flask_cors import CORS, cross_origin
 app = Flask(__name__, static_url_path = "")
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+cors = CORS(app, resources={r"*": {"origins": "null", "supports_credentials": True}})
+logging.getLogger('flask_cors').lev = logging.DEBUG
 
 # Parametres specifiques a ce webservice
 wsName = 'ws_filtrunwrInterf'
 wsVersion = config['apiVersion']
-wsPortNumber = int(config['ws_filtrunwrInterf_PN'])
+wsPortNumber = int(config[wsName + '_PN'])
 
-app = Flask(__name__, static_url_path = "")
+#app = Flask(__name__, static_url_path = "")
 auth = HTTPBasicAuth()
 
 @auth.get_password
@@ -61,7 +62,7 @@ def get_password(username):
 def unauthorized():
     return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
     # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
-    
+
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
@@ -73,52 +74,78 @@ def not_found(error):
 @app.route('/v' + wsVersion + '/services', methods = ['GET'])
 @auth.login_required
 def get_capabilities():
-    return jsonify( { "id": "06", "label": "ForM@Ter - NSBAS API", "type": "WPS", "url": ""+ url_for("get_capabilities", _external=True) +"", "contact": "contact@poleterresolide.fr" })
+    return jsonify( { "id": "03", "label": "ForM@Ter - NSBAS API", "type": "WPS", "url": ""+ url_for("get_capabilities", _external=True) +"", "contact": "contact@poleterresolide.fr" })
+
 
 @app.route('/v' + wsVersion + '/services/'+wsName, methods = ['GET'])
 @auth.login_required
 def describe_process():
     return jsonify( {
-		  "id": ""+wsName+"",
-		  "label": "ForM@Ter/Etalab ws_filtrunwrInterf webservice",
-		  "description": "Unwraps interferograms.",
-		  "inputs":  [
-		              {"processToken" : "<token>",
-                               "subSwath" : "<subswathnb>"}
-                             ], 
-                  "outputs": [
-                              {"jobId" : "<jobId>",
-                               "processToken" : "<token>",
-                               "resNames" :[
-                                            {"resName" : "<resName1>"},        
-                                            {"resName" : "<resName2>"},
-                                            {"<etc...>" : "<etc...>"}
-                                           ]
-                              }
-	                     ] 
-                    }
-		  )
+      "id": ""+wsName+"",
+      "label": "ForM@Ter/Etalab ws_filtrunwrInterf webservice",
+      "description": "Unwraps interferograms.",
+      "inputs":  [
+                  {"processToken" : "<token>",
+                                   "subSwath" : "<subswathnb>"}
+                                 ],
+                      "outputs": [
+                                  {"jobId" : "<jobId>",
+                                   "processToken" : "<token>",
+                                   "resNames" :[
+                                                {"resName" : "<resName1>"},
+                                                {"resName" : "<resName2>"},
+                                                {"<etc...>" : "<etc...>"}
+                                               ]
+                                  }
+                             ]
+                        }
+        )
 
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>/<process_token>', methods = ['GET'])
 @auth.login_required
-def get_status(job_id,process_token):
-    statusJson = getJobStatus(job_id,process_token)
-    return jsonify(statusJson)
+def get_status(job_id, process_token):
+    """ returns the status of the given process id and process token
+    :param job_id: the job id
+    :type job_id: int?
+    :param process_token: the token being queried
+    :type process_token: str (uuid)
+    :return: the status of the task
+    :rtype: str (containing a json)
+    """
+    ssh_client = None
+    try:
+        ssh_client = lws_connect.connect_with_sshconfig(config, ssh_config_file)
+    except Exception as excpt:
+        logging.critical("unable to log on %s, ABORTING", config["clstrHostName"])
+        raise excpt
+    if ssh_client is None:
+        logging.critical("unable to log on %s, ABORTING", config["clstrHostName"])
+        raise ValueError("unable to log on %s, ABORTING", config["clstrHostName"])
+    logging.info("get_status for token %s", process_token)
+    status = lws_connect.get_job_status(ssh_client, job_id)
+    ssh_client.close()
+    status_json = lws_nsbas.getJobStatus(job_id, process_token, status)
+    return jsonify(status_json)
 
 @app.route('/v' + wsVersion + '/services/'+wsName, methods = ['POST'])
 @auth.login_required
+@cross_origin({"origins": "null", "supports_credentials": True})
 def execute():
-# L'execute synchrone renvoit le resultat et la reponse http 200 : OK
-# L'execute asynchrone doit renvoyer la reponse du GetStatus et la reponse http 201 ou celle du GetResult et la reponse http 200, selon
-#
-# L'execute du webservice ws_filtrunwrInterf doit 
-#
-# 
+    """ L'execute synchrone renvoit le resultat et la reponse http 200 : OK
+     L'execute asynchrone doit renvoyer la reponse du GetStatus et la reponse http 201 ou celle du GetResult et la reponse http 200, selon
 
+     L'execute du webservice ws_coregListInterf doit
+    """
     if request.values['mode'] == "async" :
-
         # TODO : estimer dynamiquement walltime
-        process_ressources = {"nodes" : 1, "cores" : 1, "walltime" : "00:50:00", "workdir": remote_prefix}
+        process_token = request.json['processToken']
+        subswath = request.json['subSwath']
+        logging.critical("getting: token %s swath %s", str(process_token), str(subswath))
+        token_dir = remote_data_prefix + '/' + process_token
+        working_dir = token_dir + '/iw' + subswath
+        log_dir = token_dir + '/LOG'
+        process_ressources = {"nodes" : 1, "cores" : 4, "walltime" : "00:30:00",
+                "workdir": working_dir, "logdir" : log_dir}
         ret = "Error"
         error = "OK"
         job_id = -1
@@ -130,16 +157,8 @@ def execute():
         if ssh_client is None:
             logging.critical("unable to log on %s, ABORTING", config["clstrHostName"])
             raise ValueError("unable to log on %s, ABORTING", config["clstrHostName"])
-        logging.info("connection OK")
-        """"
-        TODO : élaborer la commande correspondant au ws_filtrunwrInterf
-        token_dir = config['clstrBaseDir'] + '/' + process_token
-        dem_dir = token_dir + '/DEM'
-        slc_dir = token_dir + '/SLC'
-        command = " ".join(["mkdir", dem_dir + '; ',
-                            "nsb_getDemFile.py", "fromRadarImage",
-                            slc_dir, dem_dir])
-        """
+        logging.info("con?!?jedi=1, nection OK")?!? (param iterable) ?!?jedi?!?"
+        command = " ".join('nsb_make_corr_atmo_erai.py', '-v', 'nsbas.proc'])
         try:
             logging.critical("launching command: %s", command)
             job_id = lws_connect.run_on_cluster_node(ssh_client, command, str(process_token),
@@ -150,24 +169,24 @@ def execute():
             logging.error(error)
         ssh_client.close()
 
-        # Des lors qu'il est lance, le webservice donne son jeton via son GetStatus, sans attendre d'avoir terminé
-        statusJson = getJobStatus(job_id,request.json['processToken'])
-        return jsonify(statusJson), 201        
+        # Des lors qu'il est lance, le webservice donne son jeton via son GetStatus,
+        #sans attendre d'avoir terminé
+        status = get_status(job_id, process_token)
+        logging.critical("response=%s", status)
+        return status, 201
     else :
         # En mode synchrone, le webservice donne illico sa réponse GetResult
-        # TODO : renseigner les resNames ou leur donner une valeur clairement générique
-        resultJson = { "job_id" : job_id , "processToken": request.json['processToken'] , "resNames" :[{"resName" : "f5g6t9r8s1f2g3t56r4"},{"resName" : "q2q3q69sd7f4g5g4g"}] }
-        return jsonify(resultJson), 200
+        resultJson = lws_nsbas.getJobStatus('NaN', process_token, "No sync mode allowed")
+        return resultJson, 200
 
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>/<process_token>/outputs', methods = ['GET'])
 #@auth.login_required
 def get_result(job_id,process_token):
-    # Lorsqu'il est interrogé uniquement à fin de suivi, 
-    # le webservice a besoin du job Id et, par sécurité, 
+    # Lorsqu'il est interrogé uniquement à fin de suivi,
+    # le webservice a besoin du job Id et, par sécurité,
     # du jeton de suivi du processus de calcul pour répondre
     # On les trouve dans les paramètres de l'url
-    # TODO : renseigner les resNames ou leur donner une valeur clairement générique
-    resultJson = { "job_id" : job_id , "processToken": process_token , "resNames" :[{"resName" : "f5g6t9r8s1f2g3t56r4"},{"resName" : "q2q3q69sd7f4g5g4g"}] }
+    resultJson = { "job_id" : job_id , "processToken": process_token }
     return jsonify(resultJson), 200
 
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>', methods = ['DELETE'])
@@ -176,7 +195,7 @@ def dismiss(job_id):
 # Directive prevue mais non mise en place. Informons l'interlocuteur par le code 501 : NOT IMPLEMENTED
     return jsonify( { 'job_id' : job_id , 'result': False } ), 501
 
-    
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     print "hostname=", config['wsHostName'], "port=", wsPortNumber
