fbb840760ce63bf0352fba9748035762aa2a279b
Franck <Franck@uga>
2017-04-28 12:09:32 +0200

    ws_coregListInterf.py: fixing get_status, fixing working dir issues.

--- api_nsbas/bin/ws_coregListInterf.py
+++ api_nsbas/bin/ws_coregListInterf.py
@@ -15,7 +15,6 @@ request.values est un tableau associatif qui reprend les variables transmises en
 
 import os
 import logging
-
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
 import paramiko
@@ -29,6 +28,7 @@ import lib_ws.ws_connect as lws_connect
 import parametres
 config = parametres.configdic
 remote_prefix = config["clstrBaseDir"]
+remote_data_prefix = config["clstrDataDir"]
 ssh_config_file = os.environ["HOME"] + "/" + ".ssh/config"
 
 # Preparons la connexion ssh via Paramiko
@@ -39,14 +39,15 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # A restreindre dès que l'hébergement du frontal sera connu
 from flask_cors import CORS, cross_origin
 app = Flask(__name__, static_url_path = "")
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+cors = CORS(app, resources={r"*": {"origins": "null", "supports_credentials": True}})
+logging.getLogger('flask_cors').lev = logging.DEBUG
 
 # Parametres specifiques a ce webservice
 wsName = 'ws_coregListInterf'
 wsVersion = config['apiVersion']
 wsPortNumber = int(config['ws_coregListInterf_PN'])
 
-app = Flask(__name__, static_url_path = "")
+#app = Flask(__name__, static_url_path = "")
 auth = HTTPBasicAuth()
 
 @auth.get_password
@@ -94,12 +95,33 @@ def describe_process():
 
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>/<process_token>', methods = ['GET'])
 @auth.login_required
-def get_status(job_id,process_token):
-    statusJson = getJobStatus(job_id,process_token)
-    return jsonify(statusJson)
+def get_status(job_id, process_token):
+    """ returns the status of the given process id and process token
+    :param job_id: the job id
+    :type job_id: int?
+    :param process_token: the token being queried
+    :type process_token: str (uuid)
+    :return: the status of the task
+    :rtype: str (containing a json)
+    """
+    ssh_client = None
+    try:
+        ssh_client = lws_connect.connect_with_sshconfig(config, ssh_config_file)
+    except Exception as excpt:
+        logging.critical("unable to log on %s, ABORTING", config["clstrHostName"])
+        raise excpt
+    if ssh_client is None:
+        logging.critical("unable to log on %s, ABORTING", config["clstrHostName"])
+        raise ValueError("unable to log on %s, ABORTING", config["clstrHostName"])
+    logging.info("get_status for token %s", process_token)
+    status = lws_connect.get_job_status(ssh_client, job_id)
+    ssh_client.close()
+    status_json = lws_nsbas.getJobStatus(job_id, process_token, status)
+    return jsonify(status_json)
 
 @app.route('/v' + wsVersion + '/services/'+wsName, methods = ['POST'])
 @auth.login_required
+@cross_origin({"origins": "null", "supports_credentials": True})
 def execute():
     """ L'execute synchrone renvoit le resultat et la reponse http 200 : OK
      L'execute asynchrone doit renvoyer la reponse du GetStatus et la reponse http 201 ou celle du GetResult et la reponse http 200, selon
@@ -111,8 +133,11 @@ def execute():
         process_token = request.json['processToken']
         subswath = request.json['subSwath']
         logging.critical("getting: token %s swath %s", str(process_token), str(subswath))
-        token_dir = config['clstrBaseDir'] + '/' + process_token
-        process_ressources = {"nodes" : 1, "cores" : 4, "walltime" : "10:00:00", "workdir": token_dir}
+        token_dir = remote_data_prefix + '/' + process_token
+        working_dir = token_dir + '/iw' + subswath
+        log_dir = token_dir + '/LOG'
+        process_ressources = {"nodes" : 1, "cores" : 4, "walltime" : "10:00:00",
+                "workdir": working_dir, "logdir" : log_dir}
         ret = "Error"
         error = "OK"
         job_id = -1
@@ -137,12 +162,13 @@ def execute():
         ssh_client.close()
 
         # Des lors qu'il est lance, le webservice donne son jeton via son GetStatus, sans attendre d'avoir terminé
-        statusJson = getJobStatus(job_id,request.json['processToken'])
-        return jsonify(statusJson), 201
+        status = get_status(job_id, process_token)
+        logging.critical("response=%s", status)
+        return status, 201
     else :
         # En mode synchrone, le webservice donne illico sa réponse GetResult
-        resultJson = { "job_id" : job_id , "processToken": request.json['processToken'] }
-        return jsonify(resultJson), 200
+        resultJson = lws_nsbas.getJobStatus('NaN', process_token, "No sync mode allowed")
+        return resultJson, 200
 
 @app.route('/v' + wsVersion + '/services/'+wsName+'/<int:job_id>/<process_token>/outputs', methods = ['GET'])
 #@auth.login_required
