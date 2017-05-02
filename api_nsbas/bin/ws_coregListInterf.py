a6f2676113248c93e0355569a007013fc07b6f46
Patrick HAYERE <hayere@ipgp.fr>
2017-05-02 18:08:31 +0200

    Adapted the code to the insertion of [] on each side of the request data string

--- api_nsbas/bin/ws_coregListInterf.py
+++ api_nsbas/bin/ws_coregListInterf.py
@@ -130,8 +130,8 @@ def execute():
     """
     if request.values['mode'] == "async" :
         # TODO : estimer dynamiquement walltime
-        process_token = request.json['processToken']
-        subswath = request.json['subSwath']
+        process_token = request.json[0]['processToken']
+        subswath = request.json[1]['subSwath']
         logging.critical("getting: token %s swath %s", str(process_token), str(subswath))
         token_dir = remote_data_prefix + '/' + process_token
         working_dir = token_dir + '/iw' + subswath
